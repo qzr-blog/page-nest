22dc84b6cc53a91862f4fc58170730dfc0e72200
qzr <z5021996@vip.qq.com>
2022-05-18 16:21:26 +0800

    fix(development): 修复404

--- src/script/push.ts
+++ src/script/push.ts
@@ -3,6 +3,5 @@ import axios from 'axios'
 export function pushBark(content: string) {
   const barkUrl = 'https://router.qzran.cn:8027/dJccmbJQsSiYBmyoDZ2T2f/'
   let res = barkUrl + encodeURI(content)
-  console.log('thissssssssssssss', res)
   axios.get(res)
 }
